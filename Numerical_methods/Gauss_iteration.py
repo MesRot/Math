95d71ad88193438bda421070474b7bb572453a00
MesRoto <mikael.roto@gmail.com>
2020-10-08 18:56:40 +0300

    Both algorithms working

--- Numerical_methods/Gauss_iteration.py
+++ Numerical_methods/Gauss_iteration.py
@@ -1,5 +1,8 @@
 import numpy as np
 import time
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import pandas as pd
 
 
 def back_substitution(A, b, lower=False):
@@ -17,7 +20,6 @@ def back_substitution(A, b, lower=False):
 
 
 def gauss_elimination(A, b):
-    start_time = time.time()
     n = len(A)
     x = np.zeros(n)
     for k in range(n):
@@ -27,11 +29,10 @@ def gauss_elimination(A, b):
                 A[i, l] -= r * A[k, l]
             b[i] = b[i] - r * b[k]
     x = back_substitution(A, b)
-    time_taken = time.time() - start_time
-    return x, time_taken
+    return x
 
 
-def lu_decomposition_and_solve(A, b):
+def lu_decomposition(A):
     n = len(A)
     L = np.identity(n)
     U = A.copy()
@@ -39,6 +40,12 @@ def lu_decomposition_and_solve(A, b):
         for j in range(k+1, n):
             L[j, k] = U[j, k] / U[k, k]
             U[j, k:] -= L[j, k] * U[k, k:]
+            for i in range(n):
+                n = len(A)
+    return L, U
+
+
+def solve_lu(L, U, b):
     Lstar = np.flip(L)
     bstar = np.flip(b)
     z = back_substitution(Lstar, bstar, lower=True)
@@ -47,16 +54,42 @@ def lu_decomposition_and_solve(A, b):
 
 
 def main():
-    n = 3
-    #A = np.random.uniform(low=0.5, high=20, size=(n,n))
-    #b = np.random.uniform(low=0.5, high=20, size=n)
-    A = np.asmatrix([[3, 4, 5], [6, 9, 2], [2, 5, 6]], dtype=np.float64)
-    b = np.asarray([7, 4, 5], dtype=np.float64)
-    x = lu_decomposition_and_solve(A, b.copy())
-    print(x)
-    print("B : ", b)
-    x, time = gauss_elimination(A, b)
-    print(x)
+    data = []
+    #df = pd.DataFrame(columns=columns, index=)
+    for z in tqdm(range(1, 6)):
+        for n in range(3, 200, 10):
+            A = np.random.uniform(low=0.5, high=20, size=(n, n))
+            start_time = time.time()
+            L, U = lu_decomposition(A)
+            for i in range(1, z):
+                b = np.random.uniform(low=0.5, high=20, size=n)
+                solve_lu(L, U, b)
+            total_time = time.time() - start_time
+            data.append([z, n, total_time, "LU"])
+
+            start_time = time.time()
+            for i in range(1, z):
+                b = np.random.uniform(low=0.5, high=20, size=n)
+                gauss_elimination(A, b)
+            total_time = time.time() - start_time
+            data.append([z, n, total_time, "Gauss"])
+
+    columns = ["b", "N", "Time", "Method"]
+    df = pd.DataFrame(columns=columns, data=data)
+    for i in range(1, 6):
+        data = df[(df.b == i)]
+        gauss_df = data[(data.Method == "Gauss")]
+        lu_df = data[(data.Method == "LU")]
+        plt.plot(lu_df.N, lu_df.Time, 'b-')
+        plt.plot(gauss_df.N, gauss_df.Time, 'r-')
+        plt.show()
+    print(df.head)
+    #plt.plot(matrix_sizes, gauss_times, 'b-')
+    #plt.ylabel("Time: ")
+    #plt.xlabel("Matrix size: ")
+    #plt.show()
+
+
     #print(f"time taken for {n}x{n} matrix: {time} seconds")
 
 
